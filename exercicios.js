0386979774471a238737313c82531b6578f0d308
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-14 10:46:06 -0300

    Passo 1

---
+++ exercicios.js
@@ -0,0 +1,10 @@
+const array = [1,2,3];
+const arrayDois = [4,5,6];
+
+
+const concatenar = (array,arrayDois) =>{
+    const arrayConcat = array.concat(arrayDois);
+    return arrayConcat;
+}
+
+console.log(concatenar(array,arrayDois));
