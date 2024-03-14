e6620c4188c01c6f6400278a1d13e232d571e052
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-14 12:54:36 -0300

    Passo 4

---
+++ exercicioQuatro.js
@@ -0,0 +1,5 @@
+const menuPrincipal = ['pizza calabresa', 'filé de carne','risoto'];
+const menuDeSobremesas = ['pizza doce','brownie','sorvete de coco'];
+
+const menuCompleto = menuPrincipal.concat(menuDeSobremesas);
+console.log(menuCompleto);
